163088509c29d7b13fdd60785dddb5f9e2c77840
OZ <34104076+ozeil@users.noreply.github.com>
2023-02-13 13:47:39 +0100

    add possibility to init db with more rows

--- src/main/java/com/oz/loomtest/PetRepository.java
+++ src/main/java/com/oz/loomtest/PetRepository.java
@@ -1,18 +1,23 @@
 package com.oz.loomtest;
 
 import io.agroal.api.AgroalDataSource;
+import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Row;
 import io.vertx.mutiny.sqlclient.RowSet;
+import io.vertx.mutiny.sqlclient.Tuple;
+import net.datafaker.Faker;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @ApplicationScoped
 public class PetRepository {
@@ -57,5 +62,34 @@ public class PetRepository {
             }
             return ret;
         });
+    }
+
+    void addAdditionalDBRows(@Observes StartupEvent ev, @ConfigProperty(name = "startup.additionalDBRows") boolean isInit,
+                @ConfigProperty(name = "startup.additionalDBRowsCount") int size) {
+        if (isInit) {
+            initDb(size);
+        }
+    }
+
+    private void initDb(int size) {
+        List<Tuple> pets = new ArrayList<>(size);
+        Faker faker = new Faker();
+        Random random = new Random();
+
+        var highestTypeID =  reactiveClient.query("SELECT MAX(id) FROM types LIMIT 1").
+                executeAndAwait().iterator().next().getInteger("max");
+        var highestOwnerID =  reactiveClient.query("SELECT MAX(id) FROM owners LIMIT 1").
+                executeAndAwait().iterator().next().getInteger("max");
+
+        for (int i = 0; i < size; i++) {
+            String name = faker.name().firstName();
+            LocalDateTime birthday = faker.date().birthday(0, 15).toLocalDateTime();
+            int type = random.nextInt(1, highestTypeID + 1);
+            int owner = random.nextInt(1, highestOwnerID + 1);
+            pets.add(Tuple.of(name, birthday, type, owner));
+        }
+
+        reactiveClient.preparedQuery("INSERT INTO pets(name, birth_date, type_id, owner_id) values($1, $2, $3, $4)")
+                .executeBatch(pets).await().indefinitely();
     }
 }
